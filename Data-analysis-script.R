3ae8d6ca9a7748f3bfddae9f3e48e05587bc41f6
Leonardo Silvestri <l.silvestri@unsw.edu.au>
2019-04-17 17:23:25 +1000

    Added first steps to read from file

--- Data-analysis-script.R
+++ Data-analysis-script.R
@@ -1,1 +1,17 @@
-# R script to analyse data
+# R script to analyse data from nerve recording experiments
+# Code by Leonardo Silvestri and Yuan Wei 
+
+# Packages
+#library(ggplot2)
+#library(signal)
+
+# Input variables
+dataDir <- "2019_04_12_Rabbit_experiment_5"
+dataFilename <- "2019_04_15_DVdetector.txt"
+
+# Read data from file (assumes a certain format)
+inputFile<- paste(dataDir,"/",dataFilename,sep="")
+infoString <- read.table(inputFile,skip=0, nrows = 9, sep="=", header=FALSE, stringsAsFactors = FALSE) # Read first 9 lines
+dataTime <- strptime(strsplit(infoString[[2]][[2]],"\t")[[1]][[3]],format="%d/%m/%Y %H:%M:%S") # Reads time data was collected
+dataTable <- read.table(inputFile,skip=9, sep="\t", header=FALSE)
+
